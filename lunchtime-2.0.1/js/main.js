46e1cc82767072b9a3edf50c7997a9cd67ff97a1
LucieKZK <lucie.kozik@social.aston-ecole.com>
2020-10-08 16:19:35 +0200

    Ajout de la modale d'inscription

--- lunchtime-2.0.1/js/main.js
+++ lunchtime-2.0.1/js/main.js
@@ -19,7 +19,6 @@ $(document).ready(function () {
 
     // window.addEventListener('load', ()=>{
     loadAllComponents();
-    console.log($('#monBouton'));
     // });
 
     /**MENU TAB */
@@ -142,31 +141,14 @@ $(document).ready(function () {
 
     });
 
+    console.log("Modal :", $('#myModal'));
+    console.log("Bouton :", $('#monBouton'));
 
-    // Get the modal
-    let modal = document.getElementById("myModal");
-    console.log(modal);
-    // Get the button that opens the modal
-    let btn = document.getElementById("monBouton");
-    console.log(btn);
+    $("#monBouton").click(function() {
+        $("#myModal").addClass("modal-on");
+    });
 
-    // Get the <span> element that closes the modal
-    let span = document.getElementsByClassName("close")[0];
-
-    // When the user clicks the button, open the modal 
-    btn.onclick = function () {
-        modal.style.display = "block";
-    }
-
-    // When the user clicks on <span> (x), close the modal
-    span.onclick = function () {
-        modal.style.display = "none";
-    }
-
-    // When the user clicks anywhere outside of the modal, close it
-    window.onclick = function (event) {
-        if (event.target == modal) {
-            modal.style.display = "none";
-        }
-    }
+    $("#inscription").click(function() {
+        $("#modal-inscription").addClass("modal-on");
+    })
 });
