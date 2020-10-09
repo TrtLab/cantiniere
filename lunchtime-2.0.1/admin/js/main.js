a4efb97a8fbae1ee5133134c7592740fa6d26948
RaphaelHerogue <57668172+RaphaelHerogue@users.noreply.github.com>
2020-10-09 15:04:44 +0200

    page config / modal crediter

--- lunchtime-2.0.1/admin/js/main.js
+++ lunchtime-2.0.1/admin/js/main.js
@@ -1,22 +1,48 @@
-let crediter = document.getElementById("crediter");
-let solder = document.getElementById("solder");
 let button = document.querySelector("button");
-let modalCrediter = document.getElementById("modalCrediter");
 let span = document.getElementsByClassName("close")[0];
 
 button.addEventListener("click", function() {
   let id = this.id;
-  id.onclick = function() {
-    modalCrediter.style.display = "block";
-  }
+  let btnActiver = document.getElementById(id);
 
-  span.onclick = function() {
-    modalCrediter.style.display = "none";
-  }
+  switch (id){
+    case "crediter":
+    console.log("dans la case crediter");
+      let modalCrediter = document.getElementById("modalCrediter");
+      btnActiver.onclick = function() {
+        modalCrediter.style.display = "block";
+      }
 
-  window.onclick = function(event) {
-    if (event.target == modalCrediter) {
-      modalCrediter.style.display = "none";
-    }
+      span.onclick = function() {
+        modalCrediter.style.display = "none";
+      }
+
+      window.onclick = function(event) {
+        if (event.target == modalCrediter) {
+          modalCrediter.style.display = "none";
+        }
+      }
+      break;
+
+      case "solder":
+      console.log("dans la case solder");
+      let modalSolder = document.getElementById("modalSolder");
+      btnActiver.onclick = function() {
+        modalSolder.style.display = "block";
+      }
+
+      span.onclick = function() {
+        modalSolder.style.display = "none";
+      }
+
+      window.onclick = function(event) {
+        if (event.target == modalSolder) {
+          modalSolder.style.display = "none";
+        }
+      }
+        break;
+
+        default:
+        console.log("default switch");
   }
 });
